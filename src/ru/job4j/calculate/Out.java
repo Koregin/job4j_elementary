46d4311fc04d45c2bc677d9ecc7d7f603c2265e9
Evgeny Koregin <mnemonic78@yandex.ru>
2021-08-18 18:09:41 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/calculate/Out.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String name = "Evgeny";
+        byte age = 43;
+        String separate = " - ";
+        String id = name + separate + age + " years old";
+        System.out.println(id);
+
+        int first = 1;
+        int second = 2;
+        int onePlusTwo = first + second;
+        System.out.println("1 + 2 = " + onePlusTwo);
+    }
+}
