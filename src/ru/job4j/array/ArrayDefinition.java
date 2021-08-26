731305f241b1b9dcc8f51269350b8a49bc4997c8
psyhoenergy <psyhoenergy@gmail.com>
2021-08-26 21:14:53 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Stiven Sigal";
+        names[1] = "Jackie Chan";
+        names[2] = "Jan Clod Vandam";
+        names[3] = "Chack Norris";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
