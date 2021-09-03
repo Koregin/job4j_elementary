112be39dfa4c939170ec965b567f3ce1b378ee9a
psyhoenergy <psyhoenergy@gmail.com>
2021-09-03 21:46:09 +0300

    Массивы. Упражнения.

---
+++ src/ru/job4j/array/SymmetricDiff.java
@@ -0,0 +1,54 @@
+package ru.job4j.array;
+
+public class SymmetricDiff {
+    public static int[] diff(int[] left, int[] right) {
+        int[] tempArr = new int[left.length + right.length];
+        int index = 0;
+        boolean match = false;
+        for (int i = 0; i < left.length; i++) {
+            match = false;
+            for (int j = 0; j < tempArr.length; j++) {
+                if (left[i] == tempArr[j]) {
+                    match = true;
+                }
+            }
+            // Если нет совпадений, то добавляем элемент в массив
+            if (!match) {
+                tempArr[index] = left[i];
+                index++;
+            }
+        }
+        match = false;
+        // Для правого множества
+        for (int i = 0; i < right.length; i++) {
+            match = false;
+            for (int j = 0; j < tempArr.length; j++) {
+                if (right[i] == tempArr[j]) {
+                    match = true;
+                    tempArr[j] = 0; // Обнуляем элемент при совпадении
+                }
+            }
+            // Если нет совпадений, то добавляем элемент в массив
+            if (!match) {
+                tempArr[index] = right[i];
+                index++;
+            }
+        }
+        // Определение размера нового массива
+        int newSize = 0;
+        for (int i = 0; i < tempArr.length; i++) {
+            if (tempArr[i] != 0) {
+                newSize++;
+            }
+        }
+        index = 0;
+        int[] newArr = new int[newSize];
+        for (int i = 0; i < tempArr.length; i++) {
+            if (tempArr[i] != 0) {
+                newArr[index] = tempArr[i];
+                index++;
+            }
+        }
+        return newArr;
+    }
+}
