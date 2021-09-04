d191187f5a01329004ccdf14e2fe99a112b0ee02
psyhoenergy <psyhoenergy@gmail.com>
2021-09-04 14:13:59 +0300

    Массивы. Упражнения.

---
+++ src/ru/job4j/array/RightShift.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class RightShift {
+    public static void shift(int[] nums, int count) {
+        while (count > 0) {
+            shift(nums);
+            count--;
+        }
+    }
+
+    // метод делает сдвиг с шагом 1
+    private static void shift(int[] nums) {
+        int temp = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            nums[i + 1] = nums[i];
+        }
+        nums[0] = temp;
+    }
+}
