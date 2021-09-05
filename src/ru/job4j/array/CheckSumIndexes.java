b5d103f1d8b25408ce57194334b67114a9f5ca5d
psyhoenergy <psyhoenergy@gmail.com>
2021-09-05 15:57:22 +0300

    Массивы. Упражнения.

--- src/ru/job4j/array/CheckSumIndexes.java
+++ src/ru/job4j/array/CheckSumIndexes.java
@@ -1,8 +1,21 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class CheckSumIndexes {
     public static int[] collectNewArray(int[][] data, int sum) {
-
-        return new int[2];
+        int[] rsl = new int[data.length * data.length];
+        int index = 0;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                if (i + j == sum) {
+                    data[i][j] = 0;
+                } else {
+                    rsl[index] = data[i][j];
+                    index++;
+                }
+            }
+        }
+        return Arrays.copyOf(rsl, index);
     }
 }
--- src/ru/job4j/array/Split.java
+++ src/ru/job4j/array/Split.java
@@ -6,6 +6,31 @@ public class Split {
     public static char[][] split(char[] str, char c) {
         char[][] result = new char[str.length / 2][];
         int commonCount = 1;
+        int index = 0;
+        int charCount = 0;
+        boolean write = false;
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] != c) {
+                if (write) {
+                    result[index][charCount] = str[i];
+                }
+                charCount++;
+                if (write && charCount == result[index].length) {
+                    write = false;
+                    index++;
+                    charCount = 0;
+                    i++;
+                }
+                if (i == str.length - 1) {
+                    i--;
+                }
+            } else if (str[i] == c || i == str.length - 1) {
+                write = true;
+                i -= charCount + 1;
+                result[index] = new char[charCount];
+                charCount = 0;
+            }
+        }
         return Arrays.copyOf(result, commonCount);
     }
 }
